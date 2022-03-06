7061e555181c2d9912a9be58bbdcbadb47a0a28d
Vesela Milcheva <93153230+veselamilcheva1@users.noreply.github.com>
2022-03-05 20:12:59 -0600

    Commit 1

---
+++ src/test/java/com/vytrack/test/US_51_accessTo_Vehicle_contracts_Vesela.java
@@ -0,0 +1,16 @@
+package com.vytrack.test;
+
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.VytrackUtils;
+
+public class US_51_accessTo_Vehicle_contracts_Vesela  {
+
+    @Test
+    public void test(){
+
+        VytrackUtils.loginAsDriver();
+    }
+
+}
