68c5af14012366b302000f4095344fe61001e6c0
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-03-08 16:24:51 -0600

    US 53 Edit_Car_Info_Icons_Margie

--- src/test/java/com/vytrack/test/US_53_Edit_Car_Info_Icons_Margie.java
+++ src/test/java/com/vytrack/test/US_53_Edit_Car_Info_Icons_Margie.java
@@ -1,4 +1,17 @@
 package com.vytrack.test;
 
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class US_53_Edit_Car_Info_Icons_Margie {
+
+ @Test
+ public void hovering(){
+
+  //go to  https://qa2.vytrack.com/user/login
+  Driver.getDriver().get(ConfigurationReader.getProperty("user.login.url"));
+
+  //click the Vehicle under the Fleet
+ }
 }
