bcd86f029b220641c8fec8901c042cd74fe3f8ab
NurcanGulkan <nurgulkan1115@gmail.com>
2022-03-14 20:02:26 -0400

    US-52 started

--- src/test/java/com/vytrack/test/US_52_ViewColumns_OnThe_VehicleModels.java
+++ src/test/java/com/vytrack/test/US_52_ViewColumns_OnThe_VehicleModels.java
@@ -1,4 +1,11 @@
 package com.vytrack.test;
 
+import org.testng.annotations.Test;
+
 public class US_52_ViewColumns_OnThe_VehicleModels {
+
+    @Test
+    public void VehicleModesPage_modules_as_sala_manager(){
+
+    }
 }
