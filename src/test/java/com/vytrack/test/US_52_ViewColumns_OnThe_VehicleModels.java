6a9c6ecf6c06f810d7311bc8b292f3e085d2c665
NurcanGulkan <nurgulkan1115@gmail.com>
2022-03-15 21:16:03 -0400

    added

--- src/test/java/com/vytrack/test/US_52_ViewColumns_OnThe_VehicleModels.java
+++ src/test/java/com/vytrack/test/US_52_ViewColumns_OnThe_VehicleModels.java
@@ -52,6 +52,77 @@ public class US_52_ViewColumns_OnThe_VehicleModels {
 
         Assert.assertEquals(actualmoduleTexts.size(),expectedmoduleTexts.size());
 
+         Driver.closeDriver();
+
+    }
+
+    @Test
+    public void VehicleModelsPage_modules_as_store_manager(){
+        VytrackUtils.loginAsStoreManger();
+        //Users click Fleet tab
+        WebElement fleetClick = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        BrowserUtils.sleep(3);
+        fleetClick.click();
+
+        //Users click Vehicle Model tab
+        WebElement vehiclesModel = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles Model']"));
+        vehiclesModel.click();
+        BrowserUtils.sleep(2);
+
+        List<WebElement> moduleElements = Driver.getDriver().findElements(By.xpath("//span[@class='grid-header-cell__label']"));
+        List<String> actualmoduleTexts = new ArrayList<>();
+
+        for (WebElement moduleElement : moduleElements) {
+            actualmoduleTexts.add(moduleElement.getText());
+            if(moduleElement.getText().equals("VENDORS")){
+                break;
+            }
+
+        }
+        List<String> expectedmoduleTexts = new ArrayList<>(Arrays.asList("MODEL NAME",
+                "MAKE",
+                "CAN BE REQUESTED",
+                "CVVI",
+                "CO2 FEE (/MONTH)",
+                "COST (DEPRECIATED)",
+                "TOTAL COST (DEPRECIATED)",
+                "CO2 EMISSIONS",
+                "FUEL TYPE",
+                "VENDORS"));
+
+        Assert.assertEquals(actualmoduleTexts, expectedmoduleTexts);
+
+        Assert.assertEquals(actualmoduleTexts.size(),expectedmoduleTexts.size());
+
+        Driver.closeDriver();
+    }
+    @Test
+    public void VehicleModelsPage_modules_as_driver() {
+        VytrackUtils.loginAsDriver();
+        //Users click Fleet tab
+        WebElement fleetClick = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
+        BrowserUtils.sleep(3);
+        fleetClick.click();
+
+        //Users click Vehicle Model tab
+        WebElement vehiclesModel = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles Model']"));
+        vehiclesModel.click();
+        BrowserUtils.sleep(2);
+
+        List<WebElement> moduleElements = Driver.getDriver().findElements(By.xpath("//span[@class='grid-header-cell__label']"));
+        List<String> actualmoduleTexts = new ArrayList<>();
+
+        for (WebElement moduleElement : moduleElements) {
+            actualmoduleTexts.add(moduleElement.getText());
+
+        }
+        List<String> expectedmoduleTexts = new ArrayList<>(Arrays.asList());
+
+
+
+        System.out.println("expectedmoduleTexts.size() = " + expectedmoduleTexts.size());
+
+        Assert.assertEquals(actualmoduleTexts.size(), expectedmoduleTexts.size());
 
 
     }
